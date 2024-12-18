cafe278304aa7c547e309c6d25b6a20d108f1b0b
aritra <aritra.bera@lawsikho.in>
2024-12-19 02:18:14 +0530

    commit

--- chapter_6-classes/class.js
+++ chapter_6-classes/class.js
@@ -7,5 +7,35 @@ var Person = /** @class */ (function () {
     return Person;
 }());
 var person = new Person();
-person.name = 'Aritra';
+person.name = "Aritra";
 console.log(person.name);
+//Member : Visibility - class member also be given special modifiers there are three main visibility modifiers in typeScripts.
+// Public - (define) allow acess to the class member from anywhere
+// privet - only allows access to the class member from within the class
+// protected - allows access to the class member from itself  and any class
+// The this keyword in a class usually refers to the instance of the class
+// Excrise -2 .......................................................................................
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.name = name;
+    }
+    Employee.prototype.getName = function () {
+        return this.name;
+    };
+    return Employee;
+}());
+var employee = new Employee("John Doe");
+console.log(employee.getName());
+//Parameter Properties - TypeScript provides a convenient way to define class members in the constructor, by adding a visibility modifiers to the parameter.
+//Exercise-3 .......................................................................................
+var Student = /** @class */ (function () {
+    function Student(name) {
+        this.name = name;
+    }
+    Student.prototype.getName = function () {
+        return this.name;
+    };
+    return Student;
+}());
+var student = new Student("Aritra");
+console.log(student.getName()); //pawan
--- chapter_6-classes/class.ts
+++ chapter_6-classes/class.ts
@@ -4,9 +4,49 @@
 
 // Excrise - 1 ...........................................................................................
 class Person {
-    name: String;
+  name: String;
 }
 
 const person = new Person();
-person.name = 'Aritra';
+person.name = "Aritra";
 console.log(person.name);
+
+//Member : Visibility - class member also be given special modifiers there are three main visibility modifiers in typeScripts.
+
+// Public - (define) allow acess to the class member from anywhere
+// privet - only allows access to the class member from within the class
+// protected - allows access to the class member from itself  and any class
+
+// The this keyword in a class usually refers to the instance of the class
+
+// Excrise -2 .......................................................................................
+class Employee {
+  private name: string;
+
+  public constructor(name: string) {
+    this.name = name;
+  }
+
+  public getName(): string {
+    return this.name;
+  }
+}
+
+const employee = new Employee("John Doe");
+console.log(employee.getName());
+
+
+//Parameter Properties - TypeScript provides a convenient way to define class members in the constructor, by adding a visibility modifiers to the parameter.
+//Exercise-3 .......................................................................................
+class Student{
+
+    public constructor( private name: string) {
+    }
+
+    public getName() {
+        return this.name;
+    }
+}
+
+const student = new Student("Aritra");
+console.log(student.getName()); 
